8ca412947df86285efb269181cad08aab993420d
Proshchenko <proshchenko78@yandex.ru>
2024-02-27 13:46:20 +0300

    feat(Matrix): add paralTranspose

---
+++ src/workers/testMatrix.cpp
@@ -0,0 +1,65 @@
+#include "testMatrix.hpp"
+#include <iostream>
+#include <vector>
+#include <future>
+
+Matrix Matrix::Transpose() const{
+    Matrix matrix = CopySizeToTranspose();
+    for(unsigned int raw=0; raw<height; raw++){
+        for(unsigned int col=0; col<width; col++){
+            matrix.data[col * matrix.width + raw] = data[raw*width + col];
+        }
+    }
+    std::cout << "!" << std::endl;
+    return matrix;
+}
+
+Matrix Matrix::ParallelTranspose() const{
+    size_t parts = 2;
+
+    Matrix matrix = CopySizeToTranspose();
+    std::vector<std::future<void>> futures;
+    size_t partSize = height/parts; //count of col in part
+    size_t startPos;
+
+    for(size_t part=0; part < parts; part++){
+        startPos = partSize * part;
+        if(part+1 == parts){
+            partSize = height - startPos;
+        }
+        futures.push_back(std::async( std::launch::async,
+                [this,&matrix,startPos,partSize](){
+                    this->WriteTransposePart(matrix, startPos, partSize);
+                }));
+    }
+    for(auto& future : futures){
+        future.get();
+    }
+    return matrix;
+}
+
+std::ostream& operator<<(std::ostream& out, const Matrix& matrix){
+    for(unsigned int raw=0; raw<matrix.height; raw++){
+        for(unsigned int col=0; col<matrix.width; col++){
+            out << matrix.data[raw * matrix.width + col] << " ";
+        }
+        out << std::endl;
+    }
+    return out;
+}
+
+Matrix Matrix::CopySizeToTranspose() const{
+    Matrix matrix;
+    matrix.height = width;
+    matrix.width = height;
+    matrix.data.reserve(data.size());
+    return matrix;
+}
+
+void Matrix::WriteTransposePart(Matrix& mtx, const size_t startPosRaw, const size_t countRaw) const{
+    for(unsigned int raw=startPosRaw; raw<startPosRaw+countRaw; raw++){
+        for(unsigned int col=0; col<width; col++){
+            mtx.data[col * mtx.width + raw] = data[raw*width + col];
+        }
+    }
+}
---
+++ src/workers/testMatrix.hpp
@@ -0,0 +1,20 @@
+#pragma once
+
+#include <vector>
+#include <fstream>
+
+struct Matrix {
+    std::vector<int> data; // инкапсулировал бы, чтобы не устроить гонку
+    unsigned width, height;// при параллельном транспонировании
+    Matrix Transpose() const;
+    Matrix ParallelTranspose() const;
+
+private:
+    Matrix CopySizeToTranspose() const;
+    void WriteTransposePart(Matrix& mtx,
+                            const size_t startPosRaw, 
+                            const size_t countRaw) const;
+};
+
+std::ostream& operator<<(std::ostream& out, const Matrix& matrix);
+
