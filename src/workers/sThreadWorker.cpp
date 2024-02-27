71501d800e5a10165998878ea293b91d36ec3c77
Proshchenko <proshchenko78@yandex.ru>
2024-02-27 13:50:41 +0300

    feat(AsynProc): naive alg with single/paral Transpose

--- src/workers/sThreadWorker.cpp
+++ src/workers/sThreadWorker.cpp
@@ -1,7 +1,13 @@
 #include "sThreadWorker.hpp"
-#include "Iworker.hpp"
 
 std::future<Matrix> SThreadWorker::AsyncProcess(Matrix& matrix){
     std::future<Matrix> future;
+    future = std::async(std::launch::async, &Matrix::Transpose, &matrix);
+    return future;
+}
 
+std::future<Matrix> SThreadWorker::AsyncParallelProcess(Matrix& matrix){
+    std::future<Matrix> future;
+    future = std::async(std::launch::async, &Matrix::ParallelTranspose, &matrix);
+    return future;
 }
--- src/workers/sThreadWorker.hpp
+++ src/workers/sThreadWorker.hpp
@@ -1,12 +1,11 @@
 #pragma once
 
 #include "Iworker.hpp"
+#include <future>
 
 class SThreadWorker : public WorkerInterface {
 public:
     std::future<Matrix> AsyncProcess(Matrix& matrix) override;
-
-private:
-    
+    std::future<Matrix> AsyncParallelProcess(Matrix& matrix) override;
 };
 
